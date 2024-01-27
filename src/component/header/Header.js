e92e93f278e2ccc40a0779438cd68ee818f09544
saparbekovvvv <hamzasparbekov7@gmail.com>
2024-01-27 19:34:32 +0600

    added

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,13 +1,12 @@
 import React from "react";
-import BooksShop from "../booksShop/BooksShop";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <div id="Header">
       <div className="container">
-        <div className="logo flex">
-          <NavLink to={"/"}>
+        <div className="flex ">
+          <NavLink className="logo" to={"/"}>
             <h1>BooksShop</h1>
           </NavLink>
           <div className="links">
